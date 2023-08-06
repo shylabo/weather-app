d46d3a55ee9dc015643e1e90c63aaa9926d80c24
shylabo <shuya.sohot@gmail.com>
2023-08-05 18:48:00 -0700

    fix: current location

--- js/_favorites.js
+++ js/_favorites.js
@@ -1,18 +1,44 @@
 // ============================ //
-//  Page loading
+//  Action handling
 // ============================ //
-// When a browser is loaded, invoke loadFavorites to get favorites list.
-window.addEventListener('load', () => {
-  loadFavorites()
-  updateFavoriteStatus()
-})
+async function favoritesHandler() {
+  const currentLocation = JSON.parse(localStorage.getItem('currentLocation'))
+  const cityName = currentLocation.name
+  let favorites = JSON.parse(localStorage.getItem('favorites')) || []
+  if (favorites.includes(cityName)) {
+    await removeFromFavorites(cityName, favorites)
+  } else {
+    await addToFavorites(cityName, favorites)
+  }
+  await updateFavoriteStatus()
+  await loadFavorites()
+}
+
+function addToFavorites(cityName, favorites) {
+  favorites.push(cityName)
+  localStorage.setItem('favorites', JSON.stringify(favorites))
+}
+
+function removeFromFavorites(cityName, favorites) {
+  if (favorites) {
+    const newFavorites = favorites.filter((favorite) => favorite !== cityName)
+    localStorage.setItem('favorites', JSON.stringify(newFavorites))
+  }
+}
+
+// ============================ //
+//  DOM Manipulation
+// ============================ //
 
 function loadFavorites() {
   // Get favorites list from local storage.
   const favorites = JSON.parse(localStorage.getItem('favorites')) || []
 
-  // Add option element in favorites dropdown.
+  // Initiate dropdown elements
   const favoritesDropdown = document.getElementById('favorites-dropdown')
+  favoritesDropdown.innerHTML = '<option value="" disabled selected>Favorite Cities</option>'
+
+  // Add option element in favorites dropdown.
   favorites.forEach((cityName) => {
     const option = document.createElement('option')
     option.value = cityName
@@ -35,30 +61,4 @@ function updateFavoriteStatus() {
   }
 }
 
-// ============================ //
-//  Action handling
-// ============================ //
-function favoritesHandler() {
-  const currentLocation = JSON.parse(localStorage.getItem('currentLocation'))
-  const cityName = currentLocation.name
-  let favorites = JSON.parse(localStorage.getItem('favorites')) || []
-  if (favorites.includes(cityName)) {
-    removeFromFavorites(cityName, favorites)
-  } else {
-    addToFavorites(cityName, favorites)
-  }
-  updateFavoriteStatus()
-  location.reload()
-}
-
-function addToFavorites(cityName, favorites) {
-  favorites.push(cityName)
-  localStorage.setItem('favorites', JSON.stringify(favorites))
-}
-
-function removeFromFavorites(cityName, favorites) {
-  if (favorites) {
-    const newFavorites = favorites.filter((favorite) => favorite !== cityName)
-    localStorage.setItem('favorites', JSON.stringify(newFavorites))
-  }
-}
+loadFavorites()
