094b0cdfa554bdca583e0599febc76ee5782bfc5
shylabo <shuya.sohot@gmail.com>
2023-08-05 22:11:21 -0700

    update: add foreacast endpoint

--- js/_daily-forecast.js
+++ js/_daily-forecast.js
@@ -1,1 +1,20 @@
-// Code here
+// ============================ //
+//  External API (OpenWeather API)
+// ============================ //
+async function fetchForecast({ lat, lon }) {
+  let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${OPEN_WEATHER_API_KEY}`
+
+  try {
+    const res = await fetch(apiUrl)
+
+    if (!res.ok) {
+      throw new Error('Failed to fetch data')
+    } else {
+      const data = await res.json()
+
+      return data
+    }
+  } catch (err) {
+    console.error(err)
+  }
+}
